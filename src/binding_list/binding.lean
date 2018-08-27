9656183e00c058bc7d15b3f8a80c4fcf0f95c9b7
Sean Leather <sean.leather@gmail.com>
2018-08-27 09:47:24 +0200

    binding: use sigma

--- src/binding_list/binding.lean
+++ src/binding_list/binding.lean
@@ -3,10 +3,11 @@ import sch
 namespace tts ------------------------------------------------------------------
 
 -- A pair of a variable and a type scheme.
-@[derive decidable_eq]
-structure binding (V : Type) : Type :=
-  (var : V)
-  (sch : sch V)
+def binding (V : Type) : Type :=
+  Σ (_ : V), sch V
+
+def binding.mk {V : Type} : V → sch V → binding V :=
+  sigma.mk
 
 infix ` :~ `:80 := binding.mk
 
@@ -16,8 +17,8 @@ namespace binding --------------------------------------------------------------
 
 -- Map a function over the type scheme of a binding.
 protected
-def map (f : tts.sch V → tts.sch V) : binding V → binding V
-  | (binding.mk x s) := binding.mk x (f s)
+def map (f : sch V → sch V) : binding V → binding V :=
+  sigma.map id (λ _, f)
 
 end /- namespace -/ binding ----------------------------------------------------
 end /- namespace -/ tts --------------------------------------------------------
